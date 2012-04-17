eaa91403eb0feae05fd942bdab41be9719ea8f36
Tim Hütz <tim@huetz.biz>
2012-04-17 16:12:52 +0200

    Added another broadcast receiver for battery level change events

---
+++ src/com/halcyonwaves/apps/energize/receivers/BatteryLevelReceiver.java
@@ -0,0 +1,21 @@
+package com.halcyonwaves.apps.energize.receivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * This class is used as a receiver for broadcasts depending the change of the
+ * battery charging level. As soon as the battery charging level changes up or
+ * downwards, this receiver gets called.
+ * 
+ * @author Tim Hütz
+ */
+public class BatteryLevelReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive( final Context context, final Intent intent ) {
+		// TODO Auto-generated method stub
+
+	}
+}
