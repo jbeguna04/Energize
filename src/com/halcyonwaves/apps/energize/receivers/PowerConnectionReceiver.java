04e35fb7a692d91cc18e9dbd3c1f99f39026c3ff
Tim Hütz <tim@huetz.biz>
2012-04-17 16:08:17 +0200

    Added the initial code for a broadcast receiver for power change events

---
+++ src/com/halcyonwaves/apps/energize/receivers/PowerConnectionReceiver.java
@@ -0,0 +1,21 @@
+package com.halcyonwaves.apps.energize.receivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * This class is used as a receiver for broadcasts depending the change of the
+ * power connections. It gets called if the user changes the way the device is
+ * powered (e.g. the user plugs or unplugs the device from a USB or charging
+ * device.
+ * 
+ * @author Tim Hütz
+ */
+public class PowerConnectionReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive( final Context arg0, final Intent arg1 ) {
+		// TODO: implement this
+	}
+}
