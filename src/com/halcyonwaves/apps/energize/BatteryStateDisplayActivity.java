948a620a8f13bf247e02ab5fcd8812319bda2ed4
Tim Hütz <tim@huetz.biz>
2012-04-17 19:38:53 +0200

    Added code to query the battery state and show it on the main activity

--- src/com/halcyonwaves/apps/energize/BatteryStateDisplayActivity.java
+++ src/com/halcyonwaves/apps/energize/BatteryStateDisplayActivity.java
@@ -1,7 +1,13 @@
 package com.halcyonwaves.apps.energize;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 /**
  * This class defines the behavior of the first activity the user sees after he
@@ -12,9 +18,35 @@ import android.os.Bundle;
  */
 public class BatteryStateDisplayActivity extends Activity {
 
+	private TextView batteryPercentage = null;
+
 	@Override
 	protected void onCreate( final Bundle savedInstanceState ) {
 		super.onCreate( savedInstanceState );
 		this.setContentView( R.layout.activity_batterystatedisplay );
+
+		//
+		this.batteryPercentage = (TextView) this.findViewById( R.id.tv_battery_pct );
+
+		// now we can update the battery information for displaying them
+		this.updateBatteryInformation();
+	}
+
+	private void updateBatteryInformation() {
+		BroadcastReceiver batteryLevelReceiver = new BroadcastReceiver() {
+
+			public void onReceive( Context context, Intent intent ) {
+				context.unregisterReceiver( this );
+				int rawlevel = intent.getIntExtra( BatteryManager.EXTRA_LEVEL, -1 );
+				int scale = intent.getIntExtra( BatteryManager.EXTRA_SCALE, -1 );
+				int level = -1;
+				if( rawlevel >= 0 && scale > 0 ) {
+					level = (rawlevel * 100) / scale;
+				}
+				batteryPercentage.setText( level + "" );
+			}
+		};
+		IntentFilter batteryLevelFilter = new IntentFilter( Intent.ACTION_BATTERY_CHANGED );
+		registerReceiver( batteryLevelReceiver, batteryLevelFilter );
 	}
 }
