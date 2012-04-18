bb6ef289b6ac2427d93e6c1c04f06d645bbe3d82
Tim Hütz <tim@huetz.biz>
2012-04-18 10:25:41 +0200

    Added a preference option to change the theme of the application

--- src/com/halcyonwaves/apps/energize/fragments/BatteryStatisticsPreferenceFragment.java
+++ src/com/halcyonwaves/apps/energize/fragments/BatteryStatisticsPreferenceFragment.java
@@ -1,6 +1,12 @@
 package com.halcyonwaves.apps.energize.fragments;
 
+import com.halcyonwaves.apps.energize.R;
+
+import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class BatteryStatisticsPreferenceFragment extends PreferenceFragment {
 }
--- src/com/halcyonwaves/apps/energize/fragments/DisplayPreferenceFragment.java
+++ src/com/halcyonwaves/apps/energize/fragments/DisplayPreferenceFragment.java
@@ -1,7 +1,14 @@
 package com.halcyonwaves.apps.energize.fragments;
 
+import com.halcyonwaves.apps.energize.R;
+
+import android.os.Bundle;
 import android.preference.PreferenceFragment;
 
 public class DisplayPreferenceFragment extends PreferenceFragment {
-
+	@Override
+	public void onCreate( Bundle savedInstanceState ) {
+		super.onCreate( savedInstanceState );
+		this.addPreferencesFromResource( R.xml.pref_display );
+	}
 }
