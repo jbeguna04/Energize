2dc477675e8a6e67928c1113adcae878b7391471
Tim Hütz <tim@huetz.biz>
2012-04-18 09:25:47 +0200

    Added two placeholder fragments for specific preferences of the app

---
+++ src/com/halcyonwaves/apps/energize/fragments/BatteryStatisticsPreferenceFragment.java
@@ -0,0 +1,7 @@
+package com.halcyonwaves.apps.energize.fragments;
+
+import android.preference.PreferenceFragment;
+
+
+public class BatteryStatisticsPreferenceFragment extends PreferenceFragment {
+}
