6d2f396d227f36145914536ad26f14e9a0da2fef
Tim Hütz <tim@huetz.biz>
2012-04-22 20:48:50 +0200

    Added an about dialog to the preferences

--- src/com/halcyonwaves/apps/energize/fragments/AboutPreferenceFragment.java
+++ src/com/halcyonwaves/apps/energize/fragments/AboutPreferenceFragment.java
@@ -1,8 +1,26 @@
 package com.halcyonwaves.apps.energize.fragments;
 
+import com.halcyonwaves.apps.energize.R;
+
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.util.Log;
 
 
 public class AboutPreferenceFragment extends PreferenceFragment {
 
+	@Override
+	public void onCreate( final Bundle savedInstanceState ) {
+		super.onCreate( savedInstanceState );
+		this.addPreferencesFromResource( R.xml.pref_about );
+		
+		try {
+			Preference applicationVersionPreference = this.findPreference( "about.app_version" );
+			applicationVersionPreference.setSummary( this.getActivity().getPackageManager().getPackageInfo(this.getActivity().getPackageName(), 0).versionName );
+		} catch( NameNotFoundException e ) {
+			Log.e( "AboutPreferenceFragment", "Cannot find the preference key for setting up the application version" );
+		}
+	}
 }
