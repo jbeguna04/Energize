9056375d348cbc3f1b229931a95547c92f01f346
Tim Hütz <tim@huetz.biz>
2012-04-30 13:15:59 +0200

    Added the preference screen for managing the battery database

--- src/com/halcyonwaves/apps/energize/fragments/BatteryStatisticsPreferenceFragment.java
+++ src/com/halcyonwaves/apps/energize/fragments/BatteryStatisticsPreferenceFragment.java
@@ -1,7 +1,15 @@
 package com.halcyonwaves.apps.energize.fragments;
 
+import com.halcyonwaves.apps.energize.R;
+
+import android.os.Bundle;
 import android.preference.PreferenceFragment;
 
 public class BatteryStatisticsPreferenceFragment extends PreferenceFragment {
 
+	@Override
+	public void onCreate( final Bundle savedInstanceState ) {
+		super.onCreate( savedInstanceState );
+		this.addPreferencesFromResource( R.xml.pref_batterystatistics );
+	}
 }
