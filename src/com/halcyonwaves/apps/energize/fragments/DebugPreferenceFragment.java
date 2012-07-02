5ebcc9cd842ed4961ece68ad5b96fa9f189bb689
Tim Hütz <tim@huetz.biz>
2012-07-02 11:33:01 +0200

    Extended the debug fragment to connect to the monitoring service

--- src/com/halcyonwaves/apps/energize/fragments/DebugPreferenceFragment.java
+++ src/com/halcyonwaves/apps/energize/fragments/DebugPreferenceFragment.java
@@ -18,17 +18,81 @@
 
 package com.halcyonwaves.apps.energize.fragments;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceFragment;
 import android.util.Log;
 
 import com.halcyonwaves.apps.energize.R;
+import com.halcyonwaves.apps.energize.services.MonitorBatteryStateService;
 
 public class DebugPreferenceFragment extends PreferenceFragment {
 	
 	private Preference sendDatabasePreference = null;
+	private Messenger monitorService = null;
+	private final Messenger monitorServiceMessanger = new Messenger( new IncomingHandler() );
+
+	class IncomingHandler extends Handler {
+
+		@Override
+		public void handleMessage( Message msg ) {
+			switch( msg.what ) {
+				default:
+					super.handleMessage( msg );
+			}
+		}
+	}
+
+	private ServiceConnection monitorServiceConnection = new ServiceConnection() {
+
+		public void onServiceConnected( ComponentName className, IBinder service ) {
+			DebugPreferenceFragment.this.monitorService = new Messenger( service );
+			try {
+				Log.d( "DebugPreferenceFragment", "Trying to connect to the battery monitoring service..." );
+				Message msg = Message.obtain( null, MonitorBatteryStateService.MSG_REGISTER_CLIENT );
+				msg.replyTo = DebugPreferenceFragment.this.monitorServiceMessanger;
+				DebugPreferenceFragment.this.monitorService.send( msg );
+			} catch( RemoteException e ) {
+				Log.e( "DebugPreferenceFragment", "Failed to connect to the battery monitoring service!" );
+			}
+		}
+
+		public void onServiceDisconnected( ComponentName className ) {
+			DebugPreferenceFragment.this.monitorService = null;
+		}
+	};
+
+	private void doBindService() {
+		this.getActivity().bindService( new Intent( this.getActivity(), MonitorBatteryStateService.class ), this.monitorServiceConnection, Context.BIND_AUTO_CREATE );
+	}
+
+	private void doUnbindService() {
+		if( this.monitorService != null ) {
+			try {
+				Message msg = Message.obtain( null, MonitorBatteryStateService.MSG_UNREGISTER_CLIENT );
+				msg.replyTo = this.monitorServiceMessanger;
+				this.monitorService.send( msg );
+			} catch( RemoteException e ) {
+			}
+		}
+		this.getActivity().unbindService( this.monitorServiceConnection );
+	}
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		this.doUnbindService();
+	}
 
 	@Override
 	public void onCreate( final Bundle savedInstanceState ) {
@@ -41,8 +105,17 @@ public class DebugPreferenceFragment extends PreferenceFragment {
 			@Override
 			public boolean onPreferenceClick( Preference preference ) {
 				Log.v( "DebugPreferenceFragment", "Prepare battery statistics database for sending via mail..." );
+				try {
+					DebugPreferenceFragment.this.monitorServiceMessanger.send(Message.obtain(null, MonitorBatteryStateService.MSG_STOP_MONITORING, 0, 0));
+					// TODO: get the path and copy the file
+					DebugPreferenceFragment.this.monitorServiceMessanger.send(Message.obtain(null, MonitorBatteryStateService.MSG_START_MONITORING, 0, 0));
+				} catch( RemoteException e ) {
+					Log.e( "DebugPreferenceFragment", "Failed to prepare battery statistics database for sending via mail!" );
+				}
 				return false;
 			}
 		} );
+		
+		this.doBindService();
 	}
 }
