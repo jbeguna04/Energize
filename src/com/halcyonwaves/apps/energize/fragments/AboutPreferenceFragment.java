19512db8d19ab13696bf1310360189ad3e0743a4
Tim Hütz <tim@huetz.biz>
2012-08-05 10:58:28 +0200

    The licence can now be displayed

--- src/com/halcyonwaves/apps/energize/fragments/AboutPreferenceFragment.java
+++ src/com/halcyonwaves/apps/energize/fragments/AboutPreferenceFragment.java
@@ -1,19 +1,11 @@
 /**
- * Energize - An Android battery monitor
- * Copyright (C) 2012 Tim Huetz
+ * Energize - An Android battery monitor Copyright (C) 2012 Tim Huetz
  * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.halcyonwaves.apps.energize.fragments;
@@ -21,7 +13,9 @@ package com.halcyonwaves.apps.energize.fragments;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
@@ -35,7 +29,8 @@ import com.halcyonwaves.apps.energize.R;
 public class AboutPreferenceFragment extends PreferenceFragment {
 
 	private Preference whatsNewPreference = null;
-	
+	private Preference showLicencePreference = null;
+
 	private void showWhatsNewDialog() {
 		LayoutInflater inflater = LayoutInflater.from( this.getActivity() );
 
@@ -64,6 +59,17 @@ public class AboutPreferenceFragment extends PreferenceFragment {
 		} catch( final NameNotFoundException e ) {
 			Log.e( "AboutPreferenceFragment", "Cannot find the preference key for setting up the application version" );
 		}
+
+		this.showLicencePreference = this.findPreference( "about.licence" );
+		this.showLicencePreference.setOnPreferenceClickListener( new OnPreferenceClickListener() {
+
+			public boolean onPreferenceClick( Preference preference ) {
+				Intent i = new Intent( Intent.ACTION_VIEW );
+				i.setData( Uri.parse( "http://www.gnu.org/copyleft/gpl.html" ) );
+				startActivity( i );
+				return false;
+			}
+		} );
 
 		this.whatsNewPreference = this.findPreference( "about.whatsnew" );
 		this.whatsNewPreference.setOnPreferenceClickListener( new OnPreferenceClickListener() {
