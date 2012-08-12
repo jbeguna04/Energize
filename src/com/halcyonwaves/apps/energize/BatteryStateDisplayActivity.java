71b072cc2f992cc5049b07f9b38b6c3ad3fe2004
Tim Hütz <tim@huetz.biz>
2012-08-12 15:37:57 +0200

    Removed the old content for the main activity

--- src/com/halcyonwaves/apps/energize/BatteryStateDisplayActivity.java
+++ src/com/halcyonwaves/apps/energize/BatteryStateDisplayActivity.java
@@ -1,19 +1,11 @@
 /**
- * Energize - An Android battery monitor
- * Copyright (C) 2012 Tim Huetz
+ * Energize - An Android battery monitor Copyright (C) 2012 Tim Huetz
  * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.halcyonwaves.apps.energize;
@@ -22,28 +14,20 @@ import com.halcyonwaves.apps.energize.services.MonitorBatteryStateService;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
-import android.content.ServiceConnection;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.content.pm.PackageInfo;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.IBinder;
-import android.os.Message;
-import android.os.Messenger;
-import android.os.RemoteException;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
 
 /**
  * This class defines the behavior of the first activity the user sees after he
@@ -54,60 +38,8 @@ import android.widget.TextView;
  */
 public class BatteryStateDisplayActivity extends Activity {
 
-	private TextView batteryPercentage = null;
-	private Messenger monitorService = null;
-	private final Messenger monitorServiceMessanger = new Messenger( new IncomingHandler() );
 	private static final String PRIVATE_PREF = "com.halcyonwaves.apps.energize";
 	private static final String VERSION_KEY = "version_number";
-
-	class IncomingHandler extends Handler {
-
-		@Override
-		public void handleMessage( Message msg ) {
-			switch( msg.what ) {
-				case MonitorBatteryStateService.MSG_REQUEST_LAST_CHARGING_PCT:
-					BatteryStateDisplayActivity.this.batteryPercentage.setText( String.valueOf( msg.arg1 ) );
-					break;
-				default:
-					super.handleMessage( msg );
-			}
-		}
-	}
-
-	private ServiceConnection monitorServiceConnection = new ServiceConnection() {
-
-		public void onServiceConnected( ComponentName className, IBinder service ) {
-			BatteryStateDisplayActivity.this.monitorService = new Messenger( service );
-			try {
-				Log.d( "BatteryStateDisplayActivity", "Trying to connect to the battery monitoring service..." );
-				Message msg = Message.obtain( null, MonitorBatteryStateService.MSG_REGISTER_CLIENT );
-				msg.replyTo = BatteryStateDisplayActivity.this.monitorServiceMessanger;
-				BatteryStateDisplayActivity.this.monitorService.send( msg );
-			} catch( RemoteException e ) {
-				Log.e( "BatteryStateDisplayActivity", "Failed to connect to the battery monitoring service!" );
-			}
-		}
-
-		public void onServiceDisconnected( ComponentName className ) {
-			BatteryStateDisplayActivity.this.monitorService = null;
-		}
-	};
-
-	private void doBindService() {
-		this.bindService( new Intent( this, MonitorBatteryStateService.class ), this.monitorServiceConnection, Context.BIND_AUTO_CREATE );
-	}
-
-	private void doUnbindService() {
-		if( this.monitorService != null ) {
-			try {
-				Message msg = Message.obtain( null, MonitorBatteryStateService.MSG_UNREGISTER_CLIENT );
-				msg.replyTo = this.monitorServiceMessanger;
-				this.monitorService.send( msg );
-			} catch( RemoteException e ) {
-			}
-		}
-		this.unbindService( this.monitorServiceConnection );
-	}
 
 	private void init() {
 		SharedPreferences sharedPref = getSharedPreferences( PRIVATE_PREF, Context.MODE_PRIVATE );
@@ -149,15 +81,9 @@ public class BatteryStateDisplayActivity extends Activity {
 	}
 
 	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		this.doUnbindService();
-	}
-
-	@Override
 	protected void onCreate( final Bundle savedInstanceState ) {
 		super.onCreate( savedInstanceState );
-		
+
 		// set the default preferences
 		PreferenceManager.setDefaultValues( this, R.xml.pref_unified, false );
 
@@ -170,12 +96,6 @@ public class BatteryStateDisplayActivity extends Activity {
 			this.getApplicationContext().startService( new Intent( this.getApplicationContext(), MonitorBatteryStateService.class ) );
 		}
 
-		// get the handles to controls we want to modify
-		this.batteryPercentage = (TextView) this.findViewById( R.id.tv_battery_pct );
-
-		//
-		this.doBindService();
-		
 		//
 		this.init();
 	}
