1e8115e1680c80d1cbb6b1edc91c928b7d75cdbc
Tim Hütz <tim@huetz.biz>
2012-08-19 20:35:29 +0200

    Fixed a bug which caused that a wrong temprature was displayed

--- app/src/com/halcyonwaves/apps/energize/fragments/OverviewFragment.java
+++ app/src/com/halcyonwaves/apps/energize/fragments/OverviewFragment.java
@@ -47,7 +47,7 @@ public class OverviewFragment extends Fragment {
 				int rawlevel = intent.getIntExtra( BatteryManager.EXTRA_LEVEL, -1 );
 				int scale = intent.getIntExtra( BatteryManager.EXTRA_SCALE, -1 );
 				int status = intent.getIntExtra( BatteryManager.EXTRA_STATUS, -1 );
-				float temp = (float) intent.getIntExtra( BatteryManager.EXTRA_TEMPERATURE, -1 );
+				float temp = ( (float) intent.getIntExtra( BatteryManager.EXTRA_TEMPERATURE, -1 ) ) / 10.0f;
 				int level = -1;
 				if( rawlevel >= 0 && scale > 0 ) {
 					level = (rawlevel * 100) / scale;
