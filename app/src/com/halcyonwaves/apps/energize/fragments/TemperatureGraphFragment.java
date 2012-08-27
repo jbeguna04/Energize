f3a21a3ce522acb5e1ecf170ed7ce7214b351303
Tim Hütz <tim@huetz.biz>
2012-08-27 17:51:02 +0200

    Disabled scaling in both graphs

--- app/src/com/halcyonwaves/apps/energize/fragments/TemperatureGraphFragment.java
+++ app/src/com/halcyonwaves/apps/energize/fragments/TemperatureGraphFragment.java
@@ -111,7 +111,7 @@ public class TemperatureGraphFragment extends Fragment {
 		final Long currentTime = System.currentTimeMillis() / 1000L;
 		graphView.addSeries( dataSet.first );
 		graphView.setScrollable( true );
-		graphView.setScalable( true );
+		graphView.setScalable( false );
 		graphView.setDrawBackground( false );
 		if( (dataSet.second + 86400L) < currentTime ) {
 			graphView.setViewPort( (currentTime - 86400), 86400 );
