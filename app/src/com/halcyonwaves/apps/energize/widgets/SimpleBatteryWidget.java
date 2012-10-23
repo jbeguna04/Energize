c3359ac28418e0376175706c288a49d48e0b33e7
Tim Hütz <tim@huetz.biz>
2012-10-23 14:00:00 -0400

    Added support for a focusable background of the widget

--- app/src/com/halcyonwaves/apps/energize/widgets/SimpleBatteryWidget.java
+++ app/src/com/halcyonwaves/apps/energize/widgets/SimpleBatteryWidget.java
@@ -1,8 +1,13 @@
 package com.halcyonwaves.apps.energize.widgets;
 
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
-
+import android.content.Context;
 
 public class SimpleBatteryWidget extends AppWidgetProvider {
 
+	public void onUpdate( Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds ) {
+
+		super.onUpdate( context, appWidgetManager, appWidgetIds );
+	}
 }
