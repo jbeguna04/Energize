507be97649b500ab8bf4c8ff0a90fcee45dcbee1
Tim Hütz <tim@huetz.biz>
2012-10-23 15:27:00 -0400

    Added Bundle support to the estimation result class

--- app/src/com/halcyonwaves/apps/energize/estimators/EstimationResult.java
+++ app/src/com/halcyonwaves/apps/energize/estimators/EstimationResult.java
@@ -1,10 +1,25 @@
 package com.halcyonwaves.apps.energize.estimators;
 
+import android.os.Bundle;
+
 public final class EstimationResult {
+
+	public static EstimationResult fromBundle( final Bundle from ) {
+		final boolean charging = from.getBoolean( "charging" );
+		final boolean valid = from.getBoolean( "isValid" );
+		final int lvl = from.getInt( "level", 0 );
+		final int min = from.getInt( "minutes", 0 );
+		if( valid ) {
+			return new EstimationResult( min, lvl, charging );
+		} else {
+			return new EstimationResult();
+		}
+	}
 
 	public final boolean charging;
 	public final boolean isValid;
 	public final int level;
+
 	public final int minutes;
 
 	public EstimationResult() {
@@ -19,6 +34,15 @@ public final class EstimationResult {
 		this.level = level;
 		this.charging = charging;
 		this.isValid = true;
+	}
+
+	public Bundle toBundle() {
+		final Bundle returnBundle = new Bundle();
+		returnBundle.putBoolean( "charging", this.charging );
+		returnBundle.putBoolean( "isValid", this.isValid );
+		returnBundle.putInt( "level", this.level );
+		returnBundle.putInt( "minutes", this.minutes );
+		return returnBundle;
 	}
 
 }
