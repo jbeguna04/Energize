24a42d497b916d87f807fa24c2b0b5e26b54e25d
Tim Hütz <tim@huetz.biz>
2012-11-12 11:49:10 -0500

    Prepared the code to handle the second estimation algorithm

--- app/src/com/halcyonwaves/apps/energize/estimators/BatteryEstimationMgr.java
+++ app/src/com/halcyonwaves/apps/energize/estimators/BatteryEstimationMgr.java
@@ -13,9 +13,11 @@ public class BatteryEstimationMgr {
 		final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences( context );
 		final String estimationMethod = prefs.getString( "batstatistics.usedestimator", "" );
 
-		//
+		// obtain the estimation from the estimator the user selected
 		if( 0 == "LastChangeEstimate".compareToIgnoreCase( estimationMethod ) ) {
 			return SimpleEstimationAlgorithm.getEstimation( context );
+		} else if( 0 == "LastNChangeEstimate".compareToIgnoreCase( estimationMethod ) ) {
+			return SimpleEstimationAlgorithm2.getEstimation( context );
 		}
 
 		// it seems that no time estimator could be initialized
