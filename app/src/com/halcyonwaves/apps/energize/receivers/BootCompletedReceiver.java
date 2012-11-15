cdcb1ce83104cec2153c6e7e1c45914bb738d60c
Tim Hütz <tim@huetz.biz>
2012-11-15 09:13:31 -0500

    Removed the option to disable the service after booting up the device

--- app/src/com/halcyonwaves/apps/energize/receivers/BootCompletedReceiver.java
+++ app/src/com/halcyonwaves/apps/energize/receivers/BootCompletedReceiver.java
@@ -3,19 +3,14 @@ package com.halcyonwaves.apps.energize.receivers;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
-
 import com.halcyonwaves.apps.energize.services.MonitorBatteryStateService;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive( final Context context, final Intent intent ) {
-		final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences( context );
-		if( prefs.getBoolean( "advance.start_on_boot", true ) ) {
-			context.startService( new Intent( context, MonitorBatteryStateService.class ) );
-		}
+		// start the monitoring service
+		context.startService( new Intent( context, MonitorBatteryStateService.class ) );
 	}
 
 }
