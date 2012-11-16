87cd3373e6df1e30f1f93db4938c3a0d37e0c495
Tim Hütz <tim@huetz.biz>
2012-11-16 11:48:18 -0500

    Reduced the number of stress test events

--- scripts/stressTestApp.sh
+++ scripts/stressTestApp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/Applications/AndroidSDK/platform-tools/adb shell monkey -p com.halcyonwaves.apps.energize -v 100000 -s 0
+/Applications/AndroidSDK/platform-tools/adb shell monkey -p com.halcyonwaves.apps.energize -v 1000 -s 0
