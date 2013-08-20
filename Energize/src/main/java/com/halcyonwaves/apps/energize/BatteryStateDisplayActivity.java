0a55f3c7ced354de97d384895652bda12b98c10b
Tim Hütz <tim@huetz.biz>
2013-08-20 22:39:22 +0200

    Updated the code to deal with the fragment change in the drawer

--- Energize/src/main/java/com/halcyonwaves/apps/energize/BatteryStateDisplayActivity.java
+++ Energize/src/main/java/com/halcyonwaves/apps/energize/BatteryStateDisplayActivity.java
@@ -1,11 +1,14 @@
 package com.halcyonwaves.apps.energize;
 
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -13,9 +16,8 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import com.halcyonwaves.apps.energize.R;
-
 public class BatteryStateDisplayActivity extends FragmentActivity {
+	private static final String OPENED_KEY = "OPENED_KEY";
 	private int selection = 0;
 	private int oldSelection = -1;
 	private String[] names = null;
@@ -23,6 +25,8 @@ public class BatteryStateDisplayActivity extends FragmentActivity {
 	private ActionBarDrawerToggle drawerToggle = null;
 	private DrawerLayout drawer = null;
 	private ListView navList = null;
+	private SharedPreferences prefs = null;
+	private Boolean opened = null;
 
 	@Override
 	protected void onCreate( Bundle savedInstanceState ) {
@@ -35,7 +39,29 @@ public class BatteryStateDisplayActivity extends FragmentActivity {
 		drawer = (DrawerLayout) findViewById( R.id.drawer_layout );
 		navList = (ListView) findViewById( R.id.drawer );
 		navList.setAdapter( adapter );
-		drawerToggle = new ActionBarDrawerToggle( this, drawer, R.drawable.ic_drawer, R.string.open, R.string.close );
+		drawerToggle = new ActionBarDrawerToggle( this, drawer, R.drawable.ic_drawer, R.string.open, R.string.close ) {
+			@Override
+			public void onDrawerClosed( View drawerView ) {
+				super.onDrawerClosed( drawerView );
+				updateContent();
+				invalidateOptionsMenu();
+				if ( opened != null && opened == false ) {
+					opened = true;
+					if ( prefs != null ) {
+						Editor editor = prefs.edit();
+						editor.putBoolean( OPENED_KEY, true );
+						editor.apply();
+					}
+				}
+			}
+
+			@Override
+			public void onDrawerOpened( View drawerView ) {
+				super.onDrawerOpened( drawerView );
+				getActionBar().setTitle( R.string.app_name );
+				invalidateOptionsMenu();
+			}
+		};
 		drawer.setDrawerListener( drawerToggle );
 
 		navList.setOnItemClickListener( new OnItemClickListener() {
@@ -50,6 +76,19 @@ public class BatteryStateDisplayActivity extends FragmentActivity {
 		updateContent();
 		getActionBar().setDisplayHomeAsUpEnabled( true );
 		getActionBar().setHomeButtonEnabled( true );
+
+		new Thread( new Runnable() {
+
+			@Override
+			public void run() {
+				prefs = getPreferences( MODE_PRIVATE );
+				opened = prefs.getBoolean( OPENED_KEY, false );
+				if ( opened == false ) {
+					drawer.openDrawer( navList );
+				}
+			}
+
+		} ).start();
 	}
 
 	@Override
@@ -64,6 +103,17 @@ public class BatteryStateDisplayActivity extends FragmentActivity {
 			return true;
 		}
 		return super.onOptionsItemSelected( item );
+	}
+
+	@Override
+	public boolean onPrepareOptionsMenu( Menu menu ) {
+		if ( drawer != null && navList != null ) {
+			MenuItem item = null; //menu.findItem(R.id.add);
+			if ( item != null ) {
+				item.setVisible( !drawer.isDrawerOpen( navList ) );
+			}
+		}
+		return super.onPrepareOptionsMenu( menu );
 	}
 
 	private void updateContent() {
