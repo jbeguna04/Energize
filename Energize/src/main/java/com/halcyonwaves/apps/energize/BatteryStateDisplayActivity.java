f41937dbd9713864020937c53d8c18b68288d539
Tim Hütz <tim@huetz.biz>
2013-08-21 08:55:56 +0200

    Added code to show a changelog dialog

--- Energize/src/main/java/com/halcyonwaves/apps/energize/BatteryStateDisplayActivity.java
+++ Energize/src/main/java/com/halcyonwaves/apps/energize/BatteryStateDisplayActivity.java
@@ -1,5 +1,7 @@
 package com.halcyonwaves.apps.energize;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
@@ -10,15 +12,18 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.util.Log;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.halcyonwaves.apps.energize.dialogs.ChangeLogDialog;
 import com.halcyonwaves.apps.energize.services.MonitorBatteryStateService;
 
 public class BatteryStateDisplayActivity extends FragmentActivity {
@@ -104,6 +109,10 @@ public class BatteryStateDisplayActivity extends FragmentActivity {
 			Log.v( "BatteryStateDisplayActivity", "Monitoring service is not running, starting it..." );
 			this.getApplicationContext().startService( new Intent( this.getApplicationContext(), MonitorBatteryStateService.class ) );
 		}
+
+		// show the changelog dialog
+		ChangeLogDialog changeDlg = new ChangeLogDialog( this );
+		changeDlg.show();
 	}
 
 	@Override
@@ -139,5 +148,25 @@ public class BatteryStateDisplayActivity extends FragmentActivity {
 			tx.commit();
 			oldSelection = selection;
 		}
+	}
+
+	private void showWhatsNewDialog() {
+		final LayoutInflater inflater = LayoutInflater.from( this );
+
+		final View view = inflater.inflate( R.layout.dialog_whatsnew, null );
+
+		final WebView changelogWebview = (WebView) view.findViewById( R.id.webview_whatsnew );
+		changelogWebview.loadUrl( "file:///android_asset/html/changelog.html" );
+
+		final AlertDialog.Builder builder = new AlertDialog.Builder( this );
+
+		builder.setView( view ).setTitle( R.string.dialog_title_whatsnew ).setPositiveButton( android.R.string.ok, new DialogInterface.OnClickListener() {
+
+			public void onClick( final DialogInterface dialog, final int which ) {
+				dialog.dismiss();
+			}
+		} );
+
+		builder.create().show();
 	}
 }
