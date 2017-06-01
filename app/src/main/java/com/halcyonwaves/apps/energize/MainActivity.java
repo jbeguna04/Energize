ffae7fa28772348b796c6696a0e527aad60b93c8
Tim Hütz <tim@huetz.biz>
2017-06-01 21:08:07 +0200

    Ensure we can open the settings dialog

--- app/src/main/java/com/halcyonwaves/apps/energize/MainActivity.java
+++ app/src/main/java/com/halcyonwaves/apps/energize/MainActivity.java
@@ -1,10 +1,10 @@
 package com.halcyonwaves.apps.energize;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
 		implements NavigationView.OnNavigationItemSelectedListener {
@@ -68,6 +69,8 @@ public class MainActivity extends AppCompatActivity
 
 		//noinspection SimplifiableIfStatement
 		if (id == R.id.menu_preferences) {
+			Intent settingsIntent = new Intent(this, SettingsActivity.class);
+			startActivity(settingsIntent);
 			return true;
 		}
 
