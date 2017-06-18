181591c89cabd906d79b082012ea9993d41a86d7
Tim Hütz <tim@huetz.biz>
2017-06-18 19:46:49 +0200

    Fix the emulator charging script

--- scripts/setEmulatorPowerCapacity.sh
+++ scripts/setEmulatorPowerCapacity.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+TOKEN=`cat ~/.emulator_console_auth_token`
 expect << EOF
 spawn telnet localhost 5554
+expect "OK"
+send -- "auth $TOKEN\r"
 expect "OK"
 send -- "power capacity $1\r"
 expect "OK"
